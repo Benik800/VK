73c0ed83cc447da5f6c48411f11e5b4520c351d7
Benik800 <127986684+Benik800@users.noreply.github.com>
2024-02-24 21:04:55 +0500

    Delete VK directory

--- VK/js/main.js
+++
@@ -1,7 +0,0 @@
-const outlet = document.getElementById("outlet");
-if (outlet) outlet.addEventListener("click", mesGroupsActive);
-function mesGroupsActive() 
-{
-  const aside = document.getElementById("aside");
-  aside.classList.toggle("aside-active");
-}
